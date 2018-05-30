eefc0d454d513595bbe93a8b389cd9dd12c78daf
Rufus Maiwald <rufi@mcone.eu>
2018-05-30 18:11:23 +0200

    fixed ignored api module

--- api/src/main/java/eu/mcone/gamesystem/api/GameSystemAPI.java
+++ api/src/main/java/eu/mcone/gamesystem/api/GameSystemAPI.java
@@ -7,6 +7,7 @@ package eu.mcone.gamesystem.api;
 
 import eu.mcone.gamesystem.api.gamestate.GameStateHandler;
 import eu.mcone.gamesystem.api.player.DamageLogger;
+import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
@@ -14,86 +15,30 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
 
-public class GameSystemAPI extends JavaPlugin {
+public abstract class GameSystemAPI extends JavaPlugin {
 
-    /* Last update: 19.05.2018 / 21:20 */
-
-    private static String MainPrefix = "§8[§8GameAPI§8] ";
+    @Getter
     private static GameSystemAPI instance;
-    private static DamageLogger damageLogger;
-    private static GameStateHandler gameStateHandler;
-
-    private static HashMap<Integer, GameStateHandler> states;
-
-    /**
-     * Classes initialization constructor
-     */
-    public GameSystemAPI() {
-        states = new HashMap<>();
-    }
 
     /**
      * @param instance Sets the object 'instance' to a new GameSystem object.
      */
     protected void setInstance(final GameSystemAPI instance) {
         if (instance == null) {
-            Bukkit.getConsoleSender().sendMessage(MainPrefix + "§cGameSystem instance cannot be set twice!");
+            Bukkit.getConsoleSender().sendMessage("§cGameSystem instance cannot be set twice!");
         } else {
             GameSystemAPI.instance = instance;
         }
     }
 
     /**
-     * @param damageLogger Sets the variable 'damageLogger' to a new DamageLogger object from the class GameSystem.
-     */
-    protected void setDamageLogger(final DamageLogger damageLogger) {
-        GameSystemAPI.damageLogger = damageLogger;
-    }
-
-    /**
-     * @param gameStateHandler Sets the variable 'gameStateHandler' to a new GameStateHandler object from the class GameSystem.
-     */
-    protected static void setGameStateHandler(GameStateHandler gameStateHandler) {
-        GameSystemAPI.gameStateHandler = gameStateHandler;
-    }
-
-    /**
-     * Adds a new GameStateHandler object to the states HashMap
-     */
-    public static void createGameStateHandler() {
-        if(states.containsKey(1)) {
-            Bukkit.getConsoleSender().sendMessage(MainPrefix + "§cThere is already a GameStateHandler with the ID...");
-        } else {
-            GameSystemAPI.states.put(1, GameSystemAPI.gameStateHandler);
-        }
-    }
-
-
-    /**
      * The Default GameStateHandler return ID is 1
      */
-    public static GameStateHandler getGameStateHandler(int id) {
-        if (states.get(id) != null) {
-            return states.get(id);
-        } else {
-            Bukkit.getConsoleSender().sendMessage(MainPrefix + "§cThere is no gamestate handler with the ID '" + id + "'");
-            return null;
-        }
-    }
+    public abstract GameStateHandler getGameStateHandler();
 
     /**
      * @return Returns the DamageLogger
      */
-    public static DamageLogger getDamageLogger() {
-        return GameSystemAPI.damageLogger;
-    }
-
-
-    /**
-     * @return Returns the GameSystem instance
-     */
-    public static GameSystemAPI getInstance() {
-        return GameSystemAPI.instance;
-    }
+    public abstract DamageLogger getDamageLogger();
 
 }
--- plugin/src/main/java/eu/mcone/gamesystem/GameSystem.java
+++ plugin/src/main/java/eu/mcone/gamesystem/GameSystem.java
@@ -1,42 +1,39 @@
 package eu.mcone.gamesystem;
 
+import eu.mcone.gamesystem.api.GameSystemAPI;
 import eu.mcone.gamesystem.gamestate.GameStateHandler;
 import eu.mcone.gamesystem.listener.*;
 import eu.mcone.gamesystem.player.DamageLogger;
 import lombok.Getter;
 
-public class GameSystem extends eu.mcone.gamesystem.api.GameSystemAPI {
+public class GameSystem extends GameSystemAPI {
 
-    private static String MainPrefix = "§8[§8GameAPI§8] ";
-    private static GameStateHandler gameStateHandler;
-
+    private final static String MainPrefix = "§8[§8GameSystem§8] ";
     @Getter
-    private static DamageLogger damageLogger;
+    private GameStateHandler gameStateHandler;
+    @Getter
+    private DamageLogger damageLogger;
 
     @Override
     public void onEnable() {
         setInstance(this);
-        setGameStateHandler(new eu.mcone.gamesystem.gamestate.GameStateHandler());
-        createGameStateHandler();
 
         damageLogger = new DamageLogger();
-        setDamageLogger(damageLogger);
+        gameStateHandler = new GameStateHandler();
 
-        gameStateHandler = (GameStateHandler) getGameStateHandler(1);
-
-        getServer().getConsoleSender().sendMessage(MainPrefix + "§aBefehle und Events werden registriert...");
+        getServer().getConsoleSender().sendMessage(MainPrefix + "§aRegistering Events...");
         getServer().getPluginManager().registerEvents(new EntityDamageByEntity(), this);
         getServer().getPluginManager().registerEvents(new PlayerAchievementAwarded(), this);
         getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
         getServer().getPluginManager().registerEvents(new PlayerQuit(), this);
         getServer().getPluginManager().registerEvents(new WeatherChange(), this);
 
-        getServer().getConsoleSender().sendMessage(MainPrefix + "§aVersion §f" + this.getDescription().getVersion() + "§a wurde aktiviert...");
+        getServer().getConsoleSender().sendMessage(MainPrefix + "§aVersion §f" + this.getDescription().getVersion() + "§a enabled...");
     }
 
     @Override
     public void onDisable() {
-        getServer().getConsoleSender().sendMessage(MainPrefix + "§cPlugin wurde deaktiviert!");
+        getServer().getConsoleSender().sendMessage(MainPrefix + "§cPlugin disabled!");
     }
 
 }
