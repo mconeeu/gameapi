368cd794a2fd6f01184c89969af81c5ba6f51285
Dominik Lippl <twinsterhd@mcone.eu>
2018-08-03 13:40:27 +0200

    Add Playing Enum class to determine the maximum and minimum players in a game.

---
+++ api/src/main/java/eu/mcone/gamesystem/api/manager/Playing.java
@@ -0,0 +1,40 @@
+package eu.mcone.gamesystem.api.manager;
+
+public enum Playing {
+
+    Min_Players(0),
+    Max_Players(0);
+
+    private int value;
+
+    Playing(int value) {
+        this.value = value;
+    }
+
+    /**
+     * Returns the value of the Enum
+     * @return returns the value.
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * set the value of the Enum
+     * @param playing_enum type Playing Enum
+     * @param value        type int
+     * @return new value of the enum
+     */
+    public int setValue(Playing playing_enum, int value) {
+        return playing_enum.value = value;
+    }
+
+    /**
+     * Returns the value of the Enum as String
+     * @return returns the value
+     */
+    public String getString() {
+        return Integer.toString(this.value);
+    }
+
+}
