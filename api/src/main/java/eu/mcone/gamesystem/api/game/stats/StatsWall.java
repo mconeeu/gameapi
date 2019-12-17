dc8a1451715bdc24ff7d99d02c96f858bb8bf4fe
dominiklippl <dominik.lippl@hotmail.de>
2019-12-17 13:40:09 +0100

    LATEST BCS 10.5.3-SNAPSHOT implemented

--- api/src/main/java/eu/mcone/gamesystem/api/game/stats/StatsWall.java
+++ api/src/main/java/eu/mcone/gamesystem/api/game/stats/StatsWall.java
@@ -1,161 +1,155 @@
 package eu.mcone.gamesystem.api.game.stats;
 
 import com.mongodb.client.MongoCollection;
-import eu.mcone.coresystem.api.bukkit.CoreSystem;
 import eu.mcone.coresystem.api.bukkit.item.ItemBuilder;
 import eu.mcone.coresystem.api.bukkit.player.OfflineCorePlayer;
-import eu.mcone.coresystem.api.bukkit.world.CoreWorld;
-import eu.mcone.coresystem.api.core.exception.PlayerNotResolvedException;
-import eu.mcone.gamesystem.api.GameTemplate;
 import org.bson.Document;
-import org.bukkit.*;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.SkullType;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.Sign;
 import org.bukkit.block.Skull;
-
-import java.util.UUID;
-
-import static com.mongodb.client.model.Indexes.descending;
-import static com.mongodb.client.model.Sorts.orderBy;
 
 public class StatsWall {
 
     private MongoCollection<Document> collection;
 
     public StatsWall() {
-        collection = CoreSystem.getInstance().getStatsDB().getCollection(GameTemplate.getInstance().getGamemode().toString());
+        //collection = CoreSystem.getInstance().getStatsDB().getCollection(GameTemplate.getInstance().getGamemode().toString());
     }
 
     public void update() {
         System.out.println("Run function update");
 
-        CoreWorld lobbyWorld = CoreSystem.getInstance().getWorldManager().getWorld(GameTemplate.getInstance().getGameConfigAsClass().getLobbyWorld());
-        Location pos1 = lobbyWorld.getLocation("statsPos1");
-        Location pos2 = lobbyWorld.getLocation("statsPos2");
-
-        boolean x = false;
-        boolean z = false;
-        double x1 = pos1.getX();
-        double z1 = pos1.getZ();
-
-        long pos1X = Math.round(pos1.getX());
-        long pos2X = Math.round(pos2.getX());
-
-        long pos1Z = Math.round(pos1.getZ());
-        long pos2Z = Math.round(pos2.getZ());
-
-        if (pos1X == pos2X) {
-            x = true;
-        } else if (pos1Z == pos2Z) {
-            z = true;
-        }
-
-        int place = 1;
-
-        for (Document user : collection.find().sort(orderBy(descending("kill")))) {
-            try {
-                OfflineCorePlayer offlineCorePlayer = CoreSystem.getInstance().getOfflineCorePlayer(UUID.fromString(user.getString("uuid")));
-                OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(offlineCorePlayer.getUuid());
-
-                //Background block
-                Material material = getBlockWherePlace(place);
-
-                //South or North
-                if (z) {
-                    //Place position
-                    if (pos1.getX() > pos2.getX()) {
-                        pos1.setX(pos1.getX() - place);
-                    } else if (pos2.getX() > pos1.getX()) {
-                        pos1.setX(pos1.getX() + place);
-                    } else {
-                        break;
-                    }
-
-                    //Background block position
-                    pos1.setZ(pos1.getZ() - 1);
-                    if (!pos1.getBlock().getType().equals(Material.AIR)) {
-                        pos1.getBlock().setType(material);
-                        pos1.setZ(pos1.getZ() + 1);
-                    } else {
-                        pos1.setZ(pos1.getZ() + 2);
-                        pos1.getBlock().setType(material);
-                        pos1.setZ(pos1.getZ() - 1);
-                    }
-
-                    //Set Head
-                    pos1.setZ(pos1.getZ() - 1);
-                    if (pos1.getBlock().getType().equals(Material.AIR)) {
-                        pos1.setZ(pos1.getZ() + 1);
-                        setHead(pos1,  BlockFace.NORTH, offlinePlayer);
-                    } else {
-                        pos1.setZ(pos1.getZ() + 1);
-                        setHead(pos1,  BlockFace.SOUTH, offlinePlayer);
-                    }
-
-                    //Set sign
-                    pos1.setY(pos1.getY() - 1);
-                    if (pos1.getBlock().getType().equals(Material.WALL_SIGN)) {
-                        setSign(pos1, offlineCorePlayer, user, place);
-                        pos1.setY(pos1.getY() + 1);
-                    } else {
-                        pos1.setZ(pos1.getZ() - 2);
-                        setSign(pos1, offlineCorePlayer, user, place);
-                        pos1.setZ(pos1.getZ() + 2);
-                    }
-
-                    pos1.setX(x1);
-
-                    //West or east
-                } else if (x) {
-                    //Place position
-                    if (pos1.getZ() > pos2.getZ()) {
-                        pos1.setZ(pos1.getZ() - place);
-                    } else if (pos2.getZ() > pos1.getZ()) {
-                        pos1.setZ(pos1.getZ() + place);
-                    } else {
-                        break;
-                    }
-
-                    //Background block position
-                    pos1.setX(pos1.getX() - 1);
-                    if (!pos1.getBlock().getType().equals(Material.AIR)) {
-                        pos1.getBlock().setType(material);
-                        pos1.setX(pos1.getX() + 1);
-                    } else {
-                        pos1.setX(pos1.getX() + 2);
-                        pos1.getBlock().setType(material);
-                        pos1.setX(pos1.getX() - 1);
-                    }
-
-                    //Set Head
-                    pos1.setX(pos1.getX() - 1);
-                    if (pos1.getBlock().getType().equals(Material.AIR)) {
-                        pos1.setX(pos1.getX() + 1);
-                        setHead(pos1,  BlockFace.WEST, offlinePlayer);
-                    } else {
-                        pos1.setX(pos1.getX() + 1);
-                        setHead(pos1,  BlockFace.EAST, offlinePlayer);
-                    }
-
-                    //Set sign
-                    pos1.setY(pos1.getY() - 1);
-                    if (pos1.getBlock().getType().equals(Material.WALL_SIGN)) {
-                        setSign(pos1, offlineCorePlayer, user, place);
-                        pos1.setY(pos1.getY() + 1);
-                    } else {
-                        pos1.setX(pos1.getX() - 2);
-                        setSign(pos1, offlineCorePlayer, user, place);
-                        pos1.setX(pos1.getX() + 2);
-                    }
-
-                    pos1.setZ(z1);
-                }
-
-                place++;
-            } catch (PlayerNotResolvedException e) {
-                e.printStackTrace();
-            }
-        }
+//        CoreWorld lobbyWorld = CoreSystem.getInstance().getWorldManager().getWorld(GameTemplate.getInstance().getGameConfigAsClass().getLobbyWorld());
+//        Location pos1 = lobbyWorld.getLocation("statsPos1");
+//        Location pos2 = lobbyWorld.getLocation("statsPos2");
+//
+//        boolean x = false;
+//        boolean z = false;
+//        double x1 = pos1.getX();
+//        double z1 = pos1.getZ();
+//
+//        long pos1X = Math.round(pos1.getX());
+//        long pos2X = Math.round(pos2.getX());
+//
+//        long pos1Z = Math.round(pos1.getZ());
+//        long pos2Z = Math.round(pos2.getZ());
+//
+//        if (pos1X == pos2X) {
+//            x = true;
+//        } else if (pos1Z == pos2Z) {
+//            z = true;
+//        }
+//
+//        int place = 1;
+//
+//        for (Document user : collection.find().sort(orderBy(descending("kill")))) {
+//            try {
+//                OfflineCorePlayer offlineCorePlayer = CoreSystem.getInstance().getOfflineCorePlayer(UUID.fromString(user.getString("uuid")));
+//                OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(offlineCorePlayer.getUuid());
+//
+//                //Background block
+//                Material material = getBlockWherePlace(place);
+//
+//                //South or North
+//                if (z) {
+//                    //Place position
+//                    if (pos1.getX() > pos2.getX()) {
+//                        pos1.setX(pos1.getX() - place);
+//                    } else if (pos2.getX() > pos1.getX()) {
+//                        pos1.setX(pos1.getX() + place);
+//                    } else {
+//                        break;
+//                    }
+//
+//                    //Background block position
+//                    pos1.setZ(pos1.getZ() - 1);
+//                    if (!pos1.getBlock().getType().equals(Material.AIR)) {
+//                        pos1.getBlock().setType(material);
+//                        pos1.setZ(pos1.getZ() + 1);
+//                    } else {
+//                        pos1.setZ(pos1.getZ() + 2);
+//                        pos1.getBlock().setType(material);
+//                        pos1.setZ(pos1.getZ() - 1);
+//                    }
+//
+//                    //Set Head
+//                    pos1.setZ(pos1.getZ() - 1);
+//                    if (pos1.getBlock().getType().equals(Material.AIR)) {
+//                        pos1.setZ(pos1.getZ() + 1);
+//                        setHead(pos1,  BlockFace.NORTH, offlinePlayer);
+//                    } else {
+//                        pos1.setZ(pos1.getZ() + 1);
+//                        setHead(pos1,  BlockFace.SOUTH, offlinePlayer);
+//                    }
+//
+//                    //Set sign
+//                    pos1.setY(pos1.getY() - 1);
+//                    if (pos1.getBlock().getType().equals(Material.WALL_SIGN)) {
+//                        setSign(pos1, offlineCorePlayer, user, place);
+//                        pos1.setY(pos1.getY() + 1);
+//                    } else {
+//                        pos1.setZ(pos1.getZ() - 2);
+//                        setSign(pos1, offlineCorePlayer, user, place);
+//                        pos1.setZ(pos1.getZ() + 2);
+//                    }
+//
+//                    pos1.setX(x1);
+//
+//                    //West or east
+//                } else if (x) {
+//                    //Place position
+//                    if (pos1.getZ() > pos2.getZ()) {
+//                        pos1.setZ(pos1.getZ() - place);
+//                    } else if (pos2.getZ() > pos1.getZ()) {
+//                        pos1.setZ(pos1.getZ() + place);
+//                    } else {
+//                        break;
+//                    }
+//
+//                    //Background block position
+//                    pos1.setX(pos1.getX() - 1);
+//                    if (!pos1.getBlock().getType().equals(Material.AIR)) {
+//                        pos1.getBlock().setType(material);
+//                        pos1.setX(pos1.getX() + 1);
+//                    } else {
+//                        pos1.setX(pos1.getX() + 2);
+//                        pos1.getBlock().setType(material);
+//                        pos1.setX(pos1.getX() - 1);
+//                    }
+//
+//                    //Set Head
+//                    pos1.setX(pos1.getX() - 1);
+//                    if (pos1.getBlock().getType().equals(Material.AIR)) {
+//                        pos1.setX(pos1.getX() + 1);
+//                        setHead(pos1,  BlockFace.WEST, offlinePlayer);
+//                    } else {
+//                        pos1.setX(pos1.getX() + 1);
+//                        setHead(pos1,  BlockFace.EAST, offlinePlayer);
+//                    }
+//
+//                    //Set sign
+//                    pos1.setY(pos1.getY() - 1);
+//                    if (pos1.getBlock().getType().equals(Material.WALL_SIGN)) {
+//                        setSign(pos1, offlineCorePlayer, user, place);
+//                        pos1.setY(pos1.getY() + 1);
+//                    } else {
+//                        pos1.setX(pos1.getX() - 2);
+//                        setSign(pos1, offlineCorePlayer, user, place);
+//                        pos1.setX(pos1.getX() + 2);
+//                    }
+//
+//                    pos1.setZ(z1);
+//                }
+//
+//                place++;
+//            } catch (PlayerNotResolvedException e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 
     private void setSign(final Location pos1, final OfflineCorePlayer offlinePlayer, final Document document, final int place) {
